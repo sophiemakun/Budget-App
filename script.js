7b49917ddd8079d9c99913fd9085b35b203d5d28
sophiemakun <makunsophie@gmail.com>
2022-09-28 13:46:40 +0100

    added delete functionality

--- script.js
+++ script.js
@@ -7,34 +7,56 @@ const budgetNumber = document.querySelector('.budgetnumber');
 const expensesNumber = document.querySelector('.expensesnumber');
 const balanceNumber = document.querySelector('.balancenumber');
 const expensesItem = document.querySelector(".expenses");
-const expenses = [];
+let expenses = [];
+const filters = [];
+let i = 0;
 
 function calculate() {
-    const total = expenses.reduce((accumulator, object) => {
-        return accumulator + object.amount
-    }, 0)
-    budgetNumber.innerHTML = `$ ` + budget.value;
-    expensesNumber.innerHTML = `$ ` + total;
-    const diff = budget.value - total;
-    balanceNumber.innerHTML = `$ ` + diff;
-} 
+  const total = expenses.reduce((accumulator, object) => {
+    return accumulator + object.amount;
+  }, 0);
+  budgetNumber.innerHTML = `$ ` + budget.value;
+  expensesNumber.innerHTML = `$ ` + total;
+  const diff = budget.value - total;
+  balanceNumber.innerHTML = `$ ` + diff;
+}
 
-function addExpense() {
-    const expenseItem = { expense: expense.value, amount: parseInt(amount.value) };
-    expenses.push(expenseItem);
-     expensesItem.innerHTML = `<div class="expensetitle">
-            <p>Expense Title</p>
-            ${expenses.map(
-              (exp) => `<p class='expense-name'>-${exp.expense}</p>`
-            ).join('')}
+function deleteItem(item) {
+  expenses = expenses.filter((exp) => exp.id !== item);
+  displayExpenses();
+}
+
+function displayExpenses() {
+  let display;
+  display = `
+        <div class="parent-shell">
+        <div class="expense-value">
+        
+            <h3 class="expense-title">EXPENSE TITLE</h3>
+            <h3>EXPENSE VALUE</h3>
             
         </div>
-        <div class="expensevalue">
-            <p>Expense Value</p>
-            ${expenses.map(
-              (exp) => `<p class='expense-value'>$${exp.amount}</p>`
-            ).join('')}
-        </div>`;
+            ${expenses
+              .map((item) => {
+                return `<div class="amount-budget">
+            <p class="center">- ${item.expense}</p>
+         <p  class="center">$${item.amount}</p>
+         <p class="center" onclick="deleteItem('${item.id}')"><i class="fa-solid fa-trash"></i></p>
+             </div>`;
+              })
+              .join('')}  
+        <div>`;
+  expensesItem.innerHTML = display;
+}
+
+function addExpense() {
+  const expenseItem = {
+    id: Math.random().toString(36).substring(2, 7),
+    expense: expense.value,
+    amount: parseInt(amount.value),
+  };
+  expenses.push(expenseItem);
+  displayExpenses();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
